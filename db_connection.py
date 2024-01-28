29e4e2ff5fc5b5e9dc96bba735bdcc7511da8992
salahbm <salahbm.001@gmail.com>
2024-01-28 23:51:05 +0900

    env

--- db_connection.py
+++ db_connection.py
@@ -1,20 +1,11 @@
 import pymongo
+from decouple import config
+
+
 
 try:
-    client = pymongo.MongoClient('mongodb+srv://salahbm:Bahmuhsal2001@web-ai.qxrtmxa.mongodb.net/?retryWrites=true&w=majority')
+    client = pymongo.MongoClient(config('DJANGO_DB_HOST'))
     db = client['web-ai']
-
-    if db is not None:
-        print("Connected to MongoDB Atlas")
-
-        # Perform your database operations here
-        # For example, you can retrieve a document from a collection
-        collection = db['your_collection_name']
-        document = collection.find_one({'your_key': 'your_value'})
-        print(document)
-
-    else:
-        print("Failed to get a reference to the database.")
-
+    print("Connected to MongoDB Atlas")
 except pymongo.errors.ConnectionFailure as e:
     print(f"Failed to connect to MongoDB Atlas: {e}")
